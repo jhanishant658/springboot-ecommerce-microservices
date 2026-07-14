da9439100d3e9811b46aee406138100c9fb5a324
jhanishant658 <jhanishant658@gmail.com>
2026-07-14 15:00:43 +0530

    Added Eureka Server For Connecting Microservices with Each Other

---
+++ Service_Registry/src/test/java/QuizApp/MicroService/Service_Registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package QuizApp.MicroService.Service_Registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
