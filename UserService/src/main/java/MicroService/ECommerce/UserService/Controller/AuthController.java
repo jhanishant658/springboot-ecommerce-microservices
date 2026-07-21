87301623a420340b0fdfc28236b355fe2851b625
jhanishant658 <jhanishant658@gmail.com>
2026-07-21 14:58:17 +0530

    completed all the services setted all service port : 0 for applying load balancing

--- UserService/src/main/java/MicroService/ECommerce/UserService/Controller/AuthController.java
+++ UserService/src/main/java/MicroService/ECommerce/UserService/Controller/AuthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/user")
 public class AuthController {
     private final UserService userService;
 
